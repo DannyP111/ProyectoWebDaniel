9eadc0dc56cd0d1764bb24f437b62938937789ee
Daniel Palacios <DannyP111>
2023-11-10 08:37:31 -0400

    Hice el responsive

---
+++ main.js
@@ -0,0 +1,35 @@
+
+      window.onscroll = function () {
+        scrollFunction();
+      };
+
+      function scrollFunction() {
+        if (
+          document.body.scrollTop > 50 ||
+          document.documentElement.scrollTop > 50
+        ) {
+          document.querySelector("header").style.backgroundColor = "#ececec";
+        } else {
+          document.querySelector("header").style.backgroundColor =
+            "transparent";
+        }
+      }
+   
+
+
+
+
+
+
+
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+})
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
